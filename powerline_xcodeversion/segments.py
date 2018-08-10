cc0d7070ba60c115f4abb24c994e04213f135846
Ben Chatelain <ben@octop.ad>
2018-08-10 11:06:44 -0600

    🐛 Fix exported symbol name to get segment to show

--- powerline_xcodeversion/segments.py
+++ powerline_xcodeversion/segments.py
@@ -6,16 +6,20 @@ from subprocess import PIPE, Popen
 import os, re, string
 
 
-@requires_segment_info
-class GitStatusSegment(Segment):
+#@requires_segment_info
+class XcodeVersionSegment(Segment):
 
-    def __call__(self, pl, segment_info, create_watcher):
-        return 'xcv'
+    def __call__(self, pl): #, segment_info): #, create_watcher):
+        pl.warn("I can haz log?")
+        return [{
+            'contents': 'xcv',
+            'highlight_groups': ['xcode_version', 'xcode_build'],
+        }]
         # return [
         #     {'contents': 'xcv', 'highlight_groups': ['xcode_version', 'xcode_build'], 'divider_highlight_group': 'xcode_version:divider'}
         # ]
 
 
-gitstatus = with_docstring(GitStatusSegment(),
-'''Return the currently selected Xcode version.
+xcodeversion = with_docstring(XcodeVersionSegment(),
+'''Returns the currently selected Xcode version.
 ''')
